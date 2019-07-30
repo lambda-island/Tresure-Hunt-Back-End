81199a3fa8c0f4aa17315e415c58d58d8415d92c
dislersd <dislersd@gmail.com>
2019-07-30 09:52:13 -0700

    configured env vars

--- lambda_treasure/settings.py
+++ lambda_treasure/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'smfl$jf_ltqpsp751n76r)qpdq6jwa3=bd^%_daa2z!y&pj^xp'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
