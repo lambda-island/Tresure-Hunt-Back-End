c46ea33123b3e6aeee81d2c1c745153ea846c4c2
dislersd <dislersd@gmail.com>
2019-07-30 13:09:30 -0700

    added init url in api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import init
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('init', init, name='game-init'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,19 @@
-from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello World this is working!")
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
+
+from user_profiles.models import Profile
+
+BASE_URL = 'https://lambda-treasure-hunt.herokuapp.com/api/adv/init/'
+
+
+@api_view(['GET'])
+def init(request):
+    user = Profile.objects.filter(user__username=request.user)[0]
+    headers = {
+        'Authorization': f'Token {user.api_token}'
+    }
+    response = requests.get(BASE_URL, headers=headers)
+
+    return Response(response.json())
